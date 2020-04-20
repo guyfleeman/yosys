d967ca5a66fe03bf48fa79b6ab588c2013c85fa3
Alessandro Comodi <acomodi@antmicro.com>
2020-04-20 11:16:32 +0200

    vpr: add carry_cout also for lcu
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- techlibs/xilinx/arith_map.v
+++ techlibs/xilinx/arith_map.v
@@ -39,7 +39,7 @@ module _80_xilinx_lcu (P, G, CI, CO);
 	localparam EXPLICIT_CARRY = 1'b0;
 `endif
 
-generate if (EXPLICIT_CARRY || `LUT_SIZE == 4) begin
+generate if (`LUT_SIZE == 4) begin
 
 	wire [WIDTH-1:0] C = {CO, CI};
 	wire [WIDTH-1:0] S = P & ~G;
@@ -51,6 +51,90 @@ generate if (EXPLICIT_CARRY || `LUT_SIZE == 4) begin
 			.S(S[i]),
 			.O(CO[i])
 		);
+	end endgenerate
+
+end else if (EXPLICIT_CARRY) begin
+
+	// Turns out CO and O both use [ABCD]MUX, so provide a non-congested path
+	// to carry chain by using O outputs.
+	//
+	// Registering the output of the CARRY block would prevent the need to do
+	// this, but not all designs do that.
+	//
+	// To ensure that PAD_WIDTH > 0, add 1 to Y_WIDTH.
+	localparam Y_PAD_WIDTH  = WIDTH + 1;
+	localparam CARRY4_COUNT = (Y_PAD_WIDTH + 3) / 4;
+	localparam MAX_WIDTH    = CARRY4_COUNT * 4;
+	localparam PAD_WIDTH    = MAX_WIDTH - WIDTH;
+
+	wire [MAX_WIDTH-1:0] S =  {{PAD_WIDTH{1'b0}}, P & ~G};
+	wire [MAX_WIDTH-1:0] GG = {{PAD_WIDTH{1'b0}}, G};
+	wire [MAX_WIDTH-1:0] C;
+
+	// Carry chain between CARRY4 blocks.
+	//
+	// VPR requires that the carry chain never hit the fabric. The CO input
+	// to this techmap is the carry outputs for synthesis, e.g. might hit the
+	// fabric.
+	//
+	// So we maintain two wire sets, CO_CHAIN is the carry that is for VPR,
+	// e.g. off fabric dedicated chain.
+	wire [CARRY4_COUNT-1:0] CO_CHAIN;
+	wire [CARRY4_COUNT-1:0] CO_CHAIN_PLUG;
+
+	genvar i;
+	generate for (i = 0; i < CARRY4_COUNT; i = i + 1) begin:slice
+
+		// Partially occupied CARRY4
+		if ((i+1)*4 > Y_PAD_WIDTH) begin
+
+			// First one
+			if (i == 0) begin
+				CARRY4_COUT carry4_1st_part
+				(
+				.CYINIT(CI),
+				.DI    (GG[(Y_PAD_WIDTH - 1):i*4]),
+				.S     (S [(Y_PAD_WIDTH - 1):i*4])
+				);
+			// Another one
+			end else begin
+				CARRY4_COUT carry4_part
+				(
+				.CI    (CO_CHAIN [i-1]),
+				.DI    (GG[(Y_PAD_WIDTH - 1):i*4]),
+				.S     (S [(Y_PAD_WIDTH - 1):i*4])
+				);
+			end
+
+		// Fully occupied CARRY4
+		end else begin
+
+			// First one
+			if (i == 0) begin
+				CARRY4_COUT carry4_1st_full
+				(
+				.CYINIT(CI),
+				.DI    (GG[((i+1)*4 - 1):i*4]),
+				.S     (S [((i+1)*4 - 1):i*4]),
+				.COUT(CO_CHAIN_PLUG[i])
+				);
+			// Another one
+			end else begin
+				CARRY4_COUT carry4_full
+				(
+				.CI    (CO_CHAIN[i-1]),
+				.DI    (DI[((i+1)*4 - 1):i*4]),
+				.S     (S [((i+1)*4 - 1):i*4]),
+				.COUT(CO_CHAIN_PLUG[i])
+				);
+			end
+
+			CARRY_COUT_PLUG plug(
+				.CIN(CO_CHAIN_PLUG[i]),
+				.COUT(CO_CHAIN[i])
+			);
+		end
+
 	end endgenerate
 
 end else begin
